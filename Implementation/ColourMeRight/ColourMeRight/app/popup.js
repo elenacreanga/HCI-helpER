b70b3db573500ea3027acff6763e24ef70d5936d
elena.creanga <elena.creanga.mail@gmail.com>
2016-06-01 23:17:39 +0300

    map swipe gesture to theme from reader view

--- Implementation/ColourMeRight/ColourMeRight/app/popup.js
+++ Implementation/ColourMeRight/ColourMeRight/app/popup.js
@@ -75,64 +75,73 @@ function sendFilterRequest(value) {
 //change theme
 var themes = [
     {
+        id : 0,
         name: "theme1",  
         backgroundColour: "#121212",
         textColour: "#B2B2B2"
     },
     {
+        id: 1,
         name: "theme2",
         backgroundColour: "#525252",
         textColour: "#FEFEFE"
     },
     {
+        id: 2,
         name: "theme3",
         backgroundColour: "#F9F1E4",
         textColour: "#542D1E"
     },
     {
+        id: 3,
         name: "theme4",
         backgroundColour: "#FBFBFB",
         textColour: "#111111"
     }
 ];
 
-var currentThemeId = 0;
+var currentThemeId = 3;
 
-function changeTheme(e) {
-    var filteredThemes = themes.filter(function (x) {
-        return x.name === e.target.id;
-    });
+function applyTheme() {
     chrome.tabs.executeScript(null,
         {
-            code: "document.body.style.backgroundColor='" + filteredThemes[0].backgroundColour
-              + "'; \n document.body.style.color='" + filteredThemes[0].textColour
+            code: "document.body.style.backgroundColor='" + themes[currentThemeId].backgroundColour
+              + "'; \n document.body.style.color='" + themes[currentThemeId].textColour
               + "'; \n  " +
                 "document.body.style.lineHeight='1.75em'; \n " +
                 "var paragraphs = document.getElementsByTagName('p'); for (var i in paragraphs) { \n " +
                 "if(paragraphs[i] !== undefined && paragraphs[i].style !== undefined) { \n " +
                 "if(paragraphs[i].parentElement !== undefined) { \n " +
-                "paragraphs[i].parentElement.style.backgroundColor='" + filteredThemes[0].backgroundColour + "';  \n " +
+                "paragraphs[i].parentElement.style.backgroundColor='" + themes[currentThemeId].backgroundColour + "';  \n " +
                 "if(paragraphs[i].parentElement.parentElement !== undefined) { \n " +
-                "paragraphs[i].parentElement.parentElement.style.backgroundColor='" + filteredThemes[0].backgroundColour + "'; } }\n "
-              + "paragraphs[i].style.color='" + filteredThemes[0].textColour + "'; \n " +
+                "paragraphs[i].parentElement.parentElement.style.backgroundColor='" + themes[currentThemeId].backgroundColour + "'; } }\n "
+              + "paragraphs[i].style.color='" + themes[currentThemeId].textColour + "'; \n " +
                 "paragraphs[i].style.fontSize='16px'; } } \n " +
                 "var h1s = document.getElementsByTagName('h1');  \n " +
                 "for (var i in h1s) { \n " +
                 "if(h1s[i] !== undefined && h1s[i].style !== undefined) { \n " +
-                "h1s[i].style.backgroundColor='" + filteredThemes[0].backgroundColour + "'; \n " +
-                "h1s[i].style.color='" + filteredThemes[0].textColour + "'; \n " +
+                "h1s[i].style.backgroundColor='" + themes[currentThemeId].backgroundColour + "'; \n " +
+                "h1s[i].style.color='" + themes[currentThemeId].textColour + "'; \n " +
                 "h1s[i].style.fontSize='30px'; \n " +
                 "h1s[i].style.lineHeight='34px' \n " +
                 "} }" +
                 "var h2s = document.getElementsByTagName('h2');  \n " +
                 "for (var i in h2s) { \n " +
                 "if(h2s[i] !== undefined && h2s[i].style !== undefined) { \n " +
-                "h2s[i].style.backgroundColor='" + filteredThemes[0].backgroundColour + "'; \n " +
-                "h2s[i].style.color='" + filteredThemes[0].textColour + "'; \n " +
+                "h2s[i].style.backgroundColor='" + themes[currentThemeId].backgroundColour + "'; \n " +
+                "h2s[i].style.color='" + themes[currentThemeId].textColour + "'; \n " +
                 "h2s[i].style.fontSize='20px'; \n " +
                 "h2s[i].style.lineHeight='25px' \n " +
                 "} }"
         });
+}
+
+function changeTheme(e) {
+    var filteredThemes = themes.filter(function (x) {
+        return x.name === e.target.id;
+    });
+    currentThemeId = filteredThemes[0].id;
+    applyTheme();
 }
 
 document.addEventListener('DOMContentLoaded', function () {
@@ -164,6 +173,27 @@ function updateCurrentFilterId(circle, frame) {
     }
 }
 
+function isSwipeRight(swipe) {
+    var isRight = true;
+    var dir = swipe.direction;
+    var dirStr = dir[0] > 0.8 ? 'right' : dir[0] < -0.8 ? 'left'
+                    : dir[1] > 0.8 ? 'up' : dir[1] < -0.8 ? 'down'
+                    : dir[2] > 0.8 ? 'backward' : 'forward';
+    if (dir[0] < -0.8) {
+        isRight = false;
+    }
+    return isRight;
+
+}
+
+function updateCurrentThemeId(swipe) {
+    if (isSwipeRight(swipe)) {
+        currentThemeId = (currentThemeId + 1) % 4;
+    } else {
+        currentThemeId = (currentThemeId - 1) % 4;
+    }
+}
+
 function findPinchingFingerType(hand) {
     var pincher;
     var closest = 500;
@@ -190,13 +220,16 @@ controller.addStep(function (frame) {
 
 controller.on('swipe', function (swipe, frame) {
     // Print its data when the state is start or stop
-    if (swipe.state == 'start' || swipe.state == 'stop') {
+    if (swipe.state == 'stop') {
         var dir = swipe.direction;
         var dirStr = dir[0] > 0.8 ? 'right' : dir[0] < -0.8 ? 'left'
                    : dir[1] > 0.8 ? 'up' : dir[1] < -0.8 ? 'down'
                    : dir[2] > 0.8 ? 'backward' : 'forward';
         console.log(swipe.state, swipe.type, swipe.id, dirStr,
                     'direction:', dir);
+        updateCurrentThemeId(swipe);
+        applyTheme();
+
     }
 });
 
